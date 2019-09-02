bdb5cef82fca8d24dc9b89099aae9a9ac5244014
peko_jun <jun805@gmail.com>
2019-09-03 00:47:06 +0900

    Study of cat detecter

---
+++ video.py
@@ -0,0 +1,36 @@
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+cap = cv.VideoCapture(0)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+while True:
+    # Capture frame-by-frame
+    ret, img = cap.read()
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    #edges = cv.Canny(frame,50,100)
+
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    blur = cv.GaussianBlur(gray,(5,5),0)
+    ret3,th3 = cv.threshold(blur,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+
+    # Our operations on the frame come here
+
+    frame = img
+    frame2 = gray
+    frame3 = th3
+    
+    # Display the resulting frame
+    cv.imshow('frame', frame)
+    cv.imshow('frame2', frame2)
+    cv.imshow('frame3', frame3)
+    if cv.waitKey(1) == ord('q'):
+        break
+# When everything done, release the capture
+cap.release()
+cv.destroyAllWindows()
